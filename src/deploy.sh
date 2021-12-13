00ef695c066b1bcf21cf7cf1637cae6aeca29684
Erik Pique <epique@plainconcepts.com>
2021-12-13 11:36:57 +0100

    .

--- src/deploy.sh
+++ src/deploy.sh
@@ -32,9 +32,7 @@ if [ ! "$RESOURCE_GROUP_NAME" ] || [ ! "$STORAGE_ACCOUNT_NAME" ] || [ ! "$CONTAI
   echo "$USAGE" >&2; exit 1
 fi
 
-T=tfvars_$ENVIRONMENT_NAME
-echo $T
-echo ${{ secrets[env.T] }}
+echo env.SECRET
 
 # Create Resource Group if not exist
 #az group list -o tsv | grep $RESOURCE_GROUP_NAME -q || az group create -l $LOCATION -n $RESOURCE_GROUP_NAME -o none
